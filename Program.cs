c71b9587de1e8a9b8981d8f4284de296ee264a9f
Marcelo11114 <dedosearte@hotmail.com>
2020-09-17 01:48:17 -0300

    NomeCatalogo

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace NomeCompleto
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             string Nome;
+             string Sobrenome;
+             string tecla;
+
+            do
+              {
+               Console.Clear();
+
+               Console.WriteLine("       +----------------+");
+               Console.WriteLine("       |Manipulando Nome|");
+               Console.WriteLine("       +----------------+");
+
+               Console.WriteLine("Qual seu primeiro nome?");
+               Nome = Console.ReadLine();
+               Console.WriteLine("Qual seu Sobrenome?");
+               Sobrenome = Console.ReadLine();
+
+               Console.Clear();
+                           Console.ForegroundColor = ConsoleColor.Yellow;
+               Console.Write("Nome Completo: ");
+                           Console.ResetColor();
+               Console.WriteLine($"{Nome} {Sobrenome}");
+
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+             Console.Write("Nome Catalogo: ");
+                            Console.ResetColor();
+               Console.WriteLine($"{Sobrenome.ToUpper()}, {Nome}");
+                
+                          
+               Console.WriteLine("\n Sair? (S)");
+               tecla = Console.ReadLine().ToUpper();
+              }
+            while(tecla != "S");
+         Console.Clear();    
+        }
+    }
+}
